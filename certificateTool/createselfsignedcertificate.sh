726c2d97a390544c5d16ac176338893ef6296aa7
Alexander Andriets <avandriets@gmail.com>
2019-03-16 09:55:07 +0200

    change certificates names

--- certificateTool/createselfsignedcertificate.sh
+++ certificateTool/createselfsignedcertificate.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-openssl req -new -sha256 -nodes -out ./out/server/server.csr -newkey rsa:2048 -keyout ./out/server/server.key -config <( cat server.csr.cnf )
+openssl req -new -sha256 -nodes -out ./out/tls.csr -newkey rsa:2048 -keyout ./out/tls.key -config <( cat server.csr.cnf )
 
-openssl x509 -req -in ./out/server/server.csr -CA ./out/keyCloak.pem -CAkey ./out/keyCloak.key -CAcreateserial -out ./out/server/server.crt -days 500 -sha256 -extfile v3.ext
-
+openssl x509 -req -in ./out/tls.csr -CA ./out/keyCloak.pem -CAkey ./out/keyCloak.key -CAcreateserial -out ./out/tls.crt -days 500 -sha256 -extfile v3.ext
